02105497b438e4f06eefc337b5787d477a2af38d
Владимир Козлов <kit.jke@gmail.com>
2023-09-18 16:59:45 +0300

    exceptiion

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,10 +1,11 @@
 from http import HTTPStatus
 
-from flask import jsonify, request, url_for
+from flask import abort, jsonify, request, url_for
 
 from . import app
 from .constants import REDIRECT_VIEW
 from .error_handlers import InvalidAPIUsage
+from .exceptions import AddShortException
 from .models import URLMap
 
 API_ID_NOT_FOUND = 'Указанный id не найден'
@@ -31,19 +32,21 @@ def add_url():
         raise InvalidAPIUsage(API_BODY_NOT_FOUND)
     if 'url' not in data or not data.get('url'):
         raise InvalidAPIUsage(API_URL_REQUIRED)
-    short = (
-        URLMap.get_unique_short_id()
-        if not data.get('custom_id') else data.get('custom_id')
-    )
-    url_map = URLMap.add(
-        original=data['url'],
-        short=short
-    )
+    try:
+        url_map = URLMap.add(
+            original=data['url'],
+            short=(
+                URLMap.get_unique_short_id()
+                if not data.get('custom_id') else data.get('custom_id')
+            )
+        )
+    except AddShortException as e:
+        abort(HTTPStatus.INTERNAL_SERVER_ERROR, str(e))
     return jsonify(
         {
             'url': url_map.original,
             'short_link': url_for(
-                REDIRECT_VIEW, short=short, _external=True
+                REDIRECT_VIEW, short=url_map.short, _external=True
             )
         }
     ), HTTPStatus.CREATED
